b82689fc5a0768129ee1a456ea83a801ac657aa5
tu0423 <bakiogregonnchann@gmail.com>
2022-05-24 16:24:51 +0900

    master

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+
+//高さを判断して値設定
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.crrentHeight;
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>aaaaaaaaaaaaaaaaaaOpen up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //不使用スタイルを削除
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //paddingtopにステータスバーの高さ指定して下にずらす
+    paddingTop: STATUSBAR_HEIGHT,
   },
 });
